c4bb595e449b83b74c9c8b3de7de6161bc2cc3bb
Irfansha <irfansha536@gmail.com>
2019-09-13 17:25:05 +0200

    Updated the AMO function and completed first stage of development.

--- hamiltonian_path/naive_encoding.py
+++ hamiltonian_path/naive_encoding.py
@@ -7,9 +7,7 @@
 # Takes additional graph input to add required additional clauses.
 
 # Todos:
-# 1. Add AMO constraint function
-#   - Takes a set of variables as input and appends clauses to the main output vector.
-# 2. Add ALO constraint function
+# 1. Add comments in the printing output.
 
 import sys
 
@@ -17,31 +15,48 @@ import sys
 def AMO(var):
     temp_cnf = []
     # just an example appending for now
-    for i in var:
-        temp_cnf.append(i)
-    cnf_output.append(temp_cnf)
+    for i in range(0,len(var)):
+        for j in range(i+1, len(var)):
+            cnf_output.append([-var[i],-var[j],0])
 
 
 # Takes a set of variables as input and appends the clause to the main output vector.
 def ALO(var):
+    var.append(0)
     cnf_output.append(var)
 
 def var_map(i,j,N):
     return ((i-1)*N + j)
 
+def convert(lst):
+    s = [str(i) for i in lst]
+    return ' '.join(s)
+
+def print_cnf(N):
+    print("p cnf " + str(N*N) + " " + str(len(cnf_output)))
+    for line in cnf_output:
+        print(convert(line))
+
 cnf_output = []
 
 def main(argv):
     N = int(sys.argv[1])
-    print("N = "+str(N))
-    # AMO([1,2])
-    # ALO([1,2])
+    # print("N = "+str(N))
+
+    # AMO constraints:
+    for i in range(1,N+1):
+        temp_var = []
+        for j in range(1,N+1):
+            temp_var.append(var_map(i,j,N))
+        AMO(temp_var)
+
+    # ALO constraints:
     for i in range(1,N+1):
         temp_var = []
         for j in range(1,N+1):
             temp_var.append(var_map(i,j,N))
         ALO(temp_var)
-    print(cnf_output)
+    print_cnf(N)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
